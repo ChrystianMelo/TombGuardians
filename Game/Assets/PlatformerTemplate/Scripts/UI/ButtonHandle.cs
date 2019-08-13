eeef3e933771655d85d1dcb2ebef2a9b9079adf0
chrystianMelo <chrysmelao@gmail.com>
2019-08-13 17:50:22 -0300

    Adding characters

--- Game/Assets/PlatformerTemplate/Scripts/UI/ButtonHandle.cs
+++ Game/Assets/PlatformerTemplate/Scripts/UI/ButtonHandle.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class ButtonHandle : MonoBehaviour{
+	static string character = "pattern";
 	public void ChangeScene(string sceneName){
-		Application.LoadLevel(sceneName);
+		character = sceneName;
+		Debug.Log(character);
+		Application.LoadLevel("SampleScene");//sceneName.Substring(0,(sceneName.Length-1)));
+		//scene = sceneName.Substring(0,(sceneName.Length-1));
+		//string args  = sceneName;//.Substring(sceneName.Length-1,sceneName.Length);
+		//SceneManager.LoadScene("SampleScene", args);
 	}
 }
