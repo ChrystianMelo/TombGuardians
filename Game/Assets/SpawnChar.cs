b31d0c2dea561456c6f180895a60ed7a37b9b7e0
chrystianMelo <chrysmelao@gmail.com>
2019-08-20 16:48:53 -0300

    Adding characters

--- Game/Assets/SpawnChar.cs
+++ Game/Assets/SpawnChar.cs
@@ -5,24 +5,46 @@ using UnityEngine.SceneManagement;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Platformer.Mechanics;
 
 public class SpawnChar : MonoBehaviour
 {
 
 	public GameObject[] chars = new GameObject[5]; 
-	public GameObject spawnP1;
-	private Transform p1Pos = spawnP1.transform.position;
+	[SerializeField] private GameObject spawnP1;
+	 
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 p1Pos = spawnP1.transform.position;
         StreamReader sr = new StreamReader("SceneArgs.txt");
         string line = sr.ReadLine();
+        PlayerController jogador1;
 
         switch(line) {
 
-        	case "Ace" : Instantiate(chars[0], p1Pos);
-        				break;
+        	case "Ace" : 
+        		jogador1 = Instantiate(chars[0], p1Pos, Quaternion.identity).GetComponent<PlayerController>();
+            	jogador1.playerID = 1;
+    			break;
+            case "Anny":
+                jogador1 = Instantiate(chars[1], p1Pos, Quaternion.identity).GetComponent<PlayerController>();
+                jogador1.playerID = 1;
+                break;
+            case "Balthar" : 
+        		jogador1 = Instantiate(chars[2], p1Pos, Quaternion.identity).GetComponent<PlayerController>();
+            	jogador1.playerID = 1;
+    			break;
+            case "Dion":
+                jogador1 = Instantiate(chars[3], p1Pos, Quaternion.identity).GetComponent<PlayerController>();
+                jogador1.playerID = 1;
+                break;
+            case "Viper" : 
+        		jogador1 = Instantiate(chars[4], p1Pos, Quaternion.identity).GetComponent<PlayerController>();
+            	jogador1.playerID = 1;
+    			break;
         }
+
     }
 
     // Update is called once per frame
